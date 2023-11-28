4802397b244b995efde7adc480d044e9057e8b55
Vương Việt Cường <112933836+vietcuong2004@users.noreply.github.com>
2023-11-28 20:36:12 +0700

    Update BLOPER.cpp

--- Operator/BLOPER.cpp
+++ Operator/BLOPER.cpp
@@ -2,17 +2,13 @@
 using namespace std;
 
 class Print {
-private:
-    long L;
-    long R;
-    long S;
-    long n;
-
-public:
-    Print(long n, long S);
-    bool check(long, long, long);
-    void print(long, long, long, long);
-    void printResult();
+    private:
+        long n,S,R,L;
+    public:
+        Print(long n, long S);
+        bool check(long, long, long);
+        void printOb(long, long, long, long);
+        void printResult();
 };
 
 Print::Print(long n, long S) : n(n), S(S) {
@@ -24,17 +20,17 @@ bool Print::check(long L, long R, long S) {
     return !(S > R || S < L || S == R - 2 || S == L + 2);
 }
 
-void Print::print(long L, long R, long n, long S) {
+void Print::printOb(long L, long R, long n, long S) {
     if (n == 1) {
         cout << 1;
         return;
     }
 
     if (check(L + n, R - n, S - n)) {
-        print(L + n, R - n, n - 1, S - n);
+        printOb(L + n, R - n, n - 1, S - n);
         cout << "+" << n;
     } else {
-        print(L + n, R - n, n - 1, S + n);
+        printOb(L + n, R - n, n - 1, S + n);
         cout << "-" << n;
     }
 }
@@ -43,7 +39,7 @@ void Print::printResult() {
     if ((S + R) % 2 || !check(L, R, S)) {
         cout << "Impossible";
     } else {
-        print(L, R, n, S);
+        printOb(L, R, n, S);
     }
 }
 
@@ -51,8 +47,8 @@ int main() {
     long n, S;
     cin >> n >> S;
 
-    Print printObj(n, S);
-    printObj.printResult();
+    Print a(n, S);
+    a.printResult();
 
     return 0;
 }
